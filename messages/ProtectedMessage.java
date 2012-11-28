a10cb65590a725fd070d90e51c873296833017ed
dannyiland <dannyiland@gmail.com>
2012-11-27 21:56:37 -0800

    Adding SignedMessage to ProtectedMessage Constructor
    Also added padding for ElGamal and changed AES Key to be not huge.

--- messages/ProtectedMessage.java
+++ messages/ProtectedMessage.java
@@ -2,34 +2,40 @@ package edu.ucsb.cs290.touch.to.chat.remote.messages;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SignatureException;
+
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SealedObject;
-import java.security.GeneralSecurityException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class ProtectedMessage implements Serializable {
 	private SealedObject message;
 	private SealedObject messageKey;
 
-	public ProtectedMessage(Message message, PublicKey dest, KeyPair author) throws GeneralSecurityException, IOException {
-		SignedMessage signedMessage = new SignedMessage(message, author);
+	public ProtectedMessage(SignedMessage signedMessage, PublicKey dest, KeyPair author) throws InvalidKeyException, SignatureException, NoSuchAlgorithmException, NoSuchProviderException, IOException, NoSuchPaddingException, IllegalBlockSizeException {
 		KeyGenerator kg = KeyGenerator.getInstance("AES", "SC");
 		kg.init(128);
 		Key aesKey = kg.generateKey();
 		Cipher c = Cipher.getInstance("AES","SC");
 		c.init(Cipher.ENCRYPT_MODE, aesKey);
 		this.message = new SealedObject(signedMessage, c);
-		Cipher d = Cipher.getInstance("ElGamal", "SC");
+		Cipher d = Cipher.getInstance("ElGamal/NONE/PKCS1PADDING", "SC");
 		d.init(Cipher.ENCRYPT_MODE, dest);
-		messageKey = new SealedObject(aesKey, d);
+		messageKey = new SealedObject(aesKey.getEncoded(), d);
 	}
 	
-	public SignedMessage getMessage(PrivateKey recipient, PublicKey author) throws GeneralSecurityException, IOException, ClassNotFoundException {
-		Key aesKey = (Key) messageKey.getObject(recipient,"SC");
+	public SignedMessage getMessage(PrivateKey recipient, PublicKey author) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, IOException, ClassNotFoundException {
+		Key aesKey = new SecretKeySpec((byte[]) messageKey.getObject(recipient,"SC"),"AES");
 		return (SignedMessage) message.getObject(aesKey, "SC");
 	}
 }
