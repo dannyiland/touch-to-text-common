a05de7a89cb9ae09b4703a239d31f1b652eb031b
Danny Iland <iland@cs.ucsb.edu>
2013-02-27 16:44:27 -0800

    Add Facebook ID stuff

--- register/RegisterUser.java
+++ register/RegisterUser.java
@@ -13,6 +13,7 @@ import java.security.SignedObject;
  * This object registers a device and key with the server to allow it to receive
  * messages.
  * 
+ * @author dannyiland
  * @author charlesmunger
  */
 public class RegisterUser implements Serializable {
@@ -24,13 +25,30 @@ public class RegisterUser implements Serializable {
 	private final SignedObject regId;
 	private final PublicKey publicKey;
 	private final int maxDelay;
+    private final SignedObject fbToken;
 
-	public RegisterUser(String regId, KeyPair myKeys, int maxDelay) throws IOException, GeneralSecurityException {
-		this.regId = new SignedObject(regId, myKeys.getPrivate(),
-				Signature.getInstance("DSA", "SC"));
-		this.publicKey = myKeys.getPublic();
-		this.maxDelay = maxDelay;
-	}
+    public RegisterUser(String regId, KeyPair myKeys, int maxDelay) throws IOException, GeneralSecurityException {
+	this.regId = new SignedObject(regId, myKeys.getPrivate(),
+				      Signature.getInstance("DSA", "SC"));
+	this.publicKey = myKeys.getPublic();
+	this.maxDelay = maxDelay;
+	this.fbToken = null;
+    }
+
+    public RegisterUser(String regId, String token, KeyPair myKeys, int maxDelay) throws IOException, GeneralSecurityException {
+	this.regId = new SignedObject(regId, myKeys.getPrivate(),
+				      Signature.getInstance("DSA", "SC"));
+	this.fbToken = new SignedObject(token, myKeys.getPrivate(),
+					Signature.getInstance("DSA", "SC"));
+	this.publicKey = myKeys.getPublic();
+	this.maxDelay = maxDelay;
+    }
+
+    public String getFbToken() throws IOException, ClassNotFoundException, GeneralSecurityException{
+	if (fbToken.verify(publicKey, Signature.getInstance("DSA", "SC")))
+	    return (String) fbToken.getObject();
+	throw new SignatureException();
+    }
 
 	public String getRegId() throws IOException, ClassNotFoundException, GeneralSecurityException{
 		if (regId.verify(publicKey, Signature.getInstance("DSA", "SC")))
